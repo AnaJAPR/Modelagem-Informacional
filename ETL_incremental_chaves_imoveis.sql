a7698e2b833ca69ad716eb3e544f0729895cd857
falcaoanderson <andersonfalcaosantos@gmail.com>
2024-05-27 14:51:42 -0300

    continuação

--- ETL_incremental_chaves_imoveis.sql
+++ ETL_incremental_chaves_imoveis.sql
@@ -272,3 +272,65 @@ FOR EACH ROW EXECUTE PROCEDURE audit.ins_Cliente_func();
 
 -- ### parei na linha 241 do zagi-ETL_incremental.sql
 
+
+INSERT INTO Calendario
+SELECT 
+	gen_random_uuid(),
+	a.DataCompleta,
+	a.DiaDaSemana,
+	a.DiaDoMes,
+	a.Mes,
+	a.Trimestre,
+	a.Ano
+FROM (
+	SELECT DISTINCT
+		cast(t.TransVendaData as date) as DataCompleta,
+		to_char(t.TransVendaData, 'DY') as DiaDaSemana,
+		extract(day from t.TransVendaData) as DiaDoMes,
+		to_char(t.TransVendaData, 'MM') as Mes,
+		cast(to_char(t.TransVendaData, 'Q') as int) as Trimestre,
+		extract(year from t.TransVendaData) as Ano
+	FROM 
+		oper_chaves_imoveis.TransVenda t 
+	WHERE cast(t.TransVendaData as date) not in (select DataCompleta from Calendario)
+	) as a;
+
+
+
+INSERT INTO Receita (TransComissao, TransVendaID, CalendarioKey, CorretorKey, AnuncioKey, EnderecoKey, ClienteKey)
+SELECT 
+	t.TransComissao,
+	t.TransVendaID,
+	dwcal.CalendarioKey,
+	dwcor.CorretorKey,
+	dwan.AnuncioKey,
+	dwend.EnderecoKey,
+	dwcli.ClienteKey
+FROM 
+	oper_chaves_imoveis.TransVenda t 
+	INNER JOIN oper_chaves_imoveis.ImovelTransacao it ON t.TransVendaID = it.TransVendaID
+	INNER JOIN oper_chaves_imoveis.Imovel i ON it.ImovelID = i.ImovelID
+	INNER JOIN oper_chaves_imoveis.Regiao r ON i.RegiaoID = r.RegiaoID
+	INNER JOIN oper_chaves_imoveis.Corretor c ON t.FuncCPF = c.FuncCPF
+	INNER JOIN oper_chaves_imoveis.Funcionario f ON c.FuncCPF = f.FuncCPF
+	INNER JOIN oper_chaves_imoveis.Anuncio a ON a.ImovelID = i.ImovelID AND a.FuncCPF = c.FuncCPF
+	INNER JOIN oper_chaves_imoveis.Endereco e ON i.EndID = e.EndID
+	INNER JOIN oper_chaves_imoveis.ClienteVende cv ON cv.TransVendaID = t.TransVendaID
+	INNER JOIN oper_chaves_imoveis.Cliente cli_vende ON cv.ClienteCPF = cli_vende.ClienteCPF
+	INNER JOIN oper_chaves_imoveis.ClienteCompra cc ON cc.TransVendaID = t.TransVendaID
+	INNER JOIN oper_chaves_imoveis.Cliente cli_compra ON cc.ClienteCPF = cli_compra.ClienteCPF
+	INNER JOIN Calendario dwcal ON dwcal.DataCompleta=cast(t.TransVendaData as date)
+	INNER JOIN Corretor dwcor ON dwcor.FuncCPF = c.FuncCPF
+	INNER JOIN Anuncio dwan ON dwan.ImovelID = a.ImovelID
+	INNER JOIN Endereco dwend ON dwend.EndID = e.EndID
+	INNER JOIN Cliente dwcli ON dwcli.CliComCPF = cli_compra.ClienteCPF AND dwcli.CliVenCPF = cli_vende.ClienteCPF;
+EXCEPT
+    SELECT 
+        TransComissao,
+        TransVendaID,
+        CalendarioKey,
+        CorretorKey,
+        AnuncioKey,
+        EnderecoKey,
+        ClienteKey
+    FROM dw_chaves_imoveis.Receita;
